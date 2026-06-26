a4db05eaebb5a6e3b1a28b4d1efe1213c879d301
siamparvez001 <siamparvez003@gmail.com>
2026-06-26 06:43:54 +0600

    complete find smallest number in array

---
+++ easy-problem/find the smallest integer./index.js
@@ -0,0 +1,10 @@
+// Given an array of integers your solution should find the smallest integer.
+
+function findSmallestInt(arr) {
+    
+    const smallestNumber = Math.min(...arr)
+    console.log(smallestNumber)
+
+    //   return 0;
+}
+findSmallestInt([10, -9, 30])
