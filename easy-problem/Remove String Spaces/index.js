5da001577f08763d69aa39de74afaaf86b748314
siamparvez001 <siamparvez003@gmail.com>
2026-06-26 08:33:55 +0600

    Remove String Spaces problem

---
+++ easy-problem/Remove String Spaces/index.js
@@ -0,0 +1,7 @@
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+
+function noSpace(x){
+    console.log(x.replaceAll(" ", ""));
+}
+noSpace("This is Siam Parvez")
