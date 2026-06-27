c13a1c13483329d2c2b4eb7a7d22448fb4a53e6d
siamparvez001 <siamparvez003@gmail.com>
2026-06-27 19:10:25 +0600

    solve Convert a String to a Number problem

---
+++ 8kyu/ Convert a String to a Number!/index.js
@@ -0,0 +1,8 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+
+const stringToNumber = function (str) {
+    let strToNum = parseInt(str)
+    return strToNum;
+}
+stringToNumber("12345")
