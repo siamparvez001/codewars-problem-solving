e939808a6f6de5a075d5397baa4f4ae1d981b4aa
siamparvez001 <siamparvez003@gmail.com>
2026-06-27 20:47:11 +0600

    solve Basic Mathematical Operations problem

---
+++ 8kyu/Basic Mathematical Operations/index.js
@@ -0,0 +1,22 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+
+function basicOp(operation, value1, value2) {
+    let value = `${value1} ${operation} ${value2}`
+    if (operation == '+') {
+        return (value1 + value2)
+    }
+    else if (operation == '-') {
+        return (value1 - value2)
+    }
+    else if (operation == '*') {
+        return (value1 * value2)
+    }
+    else if (operation == '/') {
+        return (value1 / value2)
+    }
+}
+basicOp('-', 4, 7)
