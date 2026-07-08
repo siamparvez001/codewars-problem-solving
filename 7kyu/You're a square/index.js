d0a16fbb38094429fa246676d4d925b6ca19af38
siamparvez001 <siamparvez003@gmail.com>
2026-07-08 17:26:59 +0600

    solve You're a square problem

---
+++ 7kyu/You're a square/index.js
@@ -0,0 +1,13 @@
+// You're a square
+
+var isSquare = function (n) {
+    if (n < 0) {
+        return false;
+    }
+    else if(Math.sqrt(n) % 1 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
