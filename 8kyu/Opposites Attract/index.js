bc4e0867096ebda679397444bf348c3532ed2b64
siamparvez001 <siamparvez003@gmail.com>
2026-07-08 16:03:34 +0600

    solve Opposites Attract problem

---
+++ 8kyu/Opposites Attract/index.js
@@ -0,0 +1,12 @@
+// Opposites Attract
+
+function lovefunc(flower1, flower2) {
+    if (flower1 % 2 == 0 && flower2 % 2 == 0 || flower1 % 2 !== 0 && flower2 % 2 !== 0) {
+        return false;
+    }
+    else if (flower1 % 2 !== 0 && flower2 % 2 == 0 || flower1 % 2 == 0 && flower2 % 2 !== 0) {
+        return true
+    }
+}
+
+lovefunc(5, 4) 
